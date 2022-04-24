b61433bcc6ddac05953a449fbd2e5653e0e1585c
Muhammad Fadhlan <fadhlan1337@gmail.com>
2022-04-24 23:19:22 +0700

    Finish responsive Heros

--- layouts/home/sections/heros.section.tsx
+++ layouts/home/sections/heros.section.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import Typography from "../../../components/base/typography/typography";
 import Elipse from "../../../components/svg-decoration/elipse";
+import screenSizes from "../../../utils/screenSizes";
+import useWindowSize from "../../../utils/useWindowSize";
 import QuotesIcon from "../components/quotes";
 
 const QuotesSubSection: React.FC = () => {
+  const { width } = useWindowSize();
+  const displaySize =
+    width! > screenSizes.md ? "display-huge" : "display-large";
+  const iconWidth = width! > screenSizes.md ? 70 : 50;
+
   return (
-    <div className={"grid justify-items-stretch max-w-3xl mx-auto"}>
-      <div className={"absolute translate-y-3 -translate-x-6"}>
+    <div className={"grid justify-items-stretch max-w-md md:max-w-3xl mx-auto"}>
+      <div
+        className={"absolute translate-y-3 -translate-x-4 md:-translate-x-6"}
+      >
         <QuotesIcon
           variant="opening"
+          width={iconWidth}
           className={"stroke-neutral-100 stroke-2"}
         />
       </div>
       <div>
         <Typography
-          Variant={"display-huge"}
+          Variant={displaySize}
           className={"font-extrabold tracking-tighter"}
         >
-          <Typography Variant={"display-huge"} Color={"primary"}>
+          <Typography Variant={displaySize} Color={"primary"}>
             creativity
           </Typography>{" "}
           is
@@ -25,7 +35,7 @@ const QuotesSubSection: React.FC = () => {
       </div>
       <div className={"justify-self-end -translate-y-5"}>
         <Typography
-          Variant={"display-huge"}
+          Variant={displaySize}
           className={"font-extrabold tracking-tighter"}
         >
           intelligence
@@ -33,15 +43,16 @@ const QuotesSubSection: React.FC = () => {
       </div>
       <div className={"relative justify-self-center -translate-y-10"}>
         <Typography
-          Variant={"display-huge"}
+          Variant={displaySize}
           Style={"italic"}
           className={"tracking-tight"}
         >
           having fun
         </Typography>
-        <div className={"absolute -right-5 -bottom-6"}>
+        <div className={"absolute -right-5 -bottom-4 md:-bottom-6"}>
           <QuotesIcon
             variant="closing"
+            width={iconWidth}
             className={"stroke-neutral-100 stroke-2"}
           />
         </div>
@@ -84,6 +95,7 @@ const HerosSection: React.FC<HerosSectionProps> = ({
             target="_blank"
             className="inline-flex align-text-bottom"
             href={"https://quoteinvestigator.com/2017/03/02/fun/#r+15588+1+5"}
+            rel="noreferrer"
           >
             <Typography Variant={"body1"} Style={"underline"}>
               <Typography Variant={"link-arrow"}>Joey Reiman(1992)</Typography>
