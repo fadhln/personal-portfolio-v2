17dc22c1d3a25b96b1e5093aa9501279ff06835b
Muhammad Fadhlan <fadhlan1337@gmail.com>
2022-04-25 22:20:58 +0700

    Finish Responsive Footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -9,40 +9,53 @@ import Typography from "../base/typography/typography";
 import Link from "next/link";
 
 const Footer: React.FC = () => {
-  // TODO Make Footer Responsive
   return (
     <footer>
-      <div className={"flex w-full h-full px-16 py-16 bg-accent-400"}>
-        <div className="flex-1">
-          <MainLogo color={"#1C211E"} width={125} />
-          <ReactMarkdown
-            components={customizableComponents("dark")}
-            className={"mt-2"}
+      <div
+        className={
+          "w-full h-full px-2 md:px-16 pt-8 pb-2 md:py-16 bg-accent-400"
+        }
+      >
+        <div className="flex">
+          <div className="flex-1">
+            <MainLogo color={"#1C211E"} width={125} />
+            <ReactMarkdown
+              components={customizableComponents("dark")}
+              className={"mt-2"}
+            >
+              {FooterData.subtitle}
+            </ReactMarkdown>
+            <ReactMarkdown
+              components={customizableComponents("dark")}
+              className={"hidden md:block mt-16"}
+            >
+              {FooterData.copyright}
+            </ReactMarkdown>
+          </div>
+          <div
+            className={"grid grid-cols-1 md:py-4 content-between text-right"}
           >
-            {FooterData.subtitle}
-          </ReactMarkdown>
-          <ReactMarkdown
-            components={customizableComponents("dark")}
-            className={"mt-16"}
-          >
-            {FooterData.copyright}
-          </ReactMarkdown>
-        </div>
-        <div className={"grid grid-cols-1 py-4 content-between text-right"}>
-          {FooterData.menu.map((menu) => {
-            return (
-              <Link href={menu.link} key={menu.content}>
-                <a>
-                  <Typography Variant="body" Color="dark" Style="underline">
-                    <Typography Variant="link" Color="dark">
-                      {menu.content}
+            {FooterData.menu.map((menu) => {
+              return (
+                <Link href={menu.link} key={menu.content}>
+                  <a>
+                    <Typography Variant="body" Color="dark" Style="underline">
+                      <Typography Variant="link" Color="dark">
+                        {menu.content}
+                      </Typography>
                     </Typography>
-                  </Typography>
-                </a>
-              </Link>
-            );
-          })}
+                  </a>
+                </Link>
+              );
+            })}
+          </div>
         </div>
+        <ReactMarkdown
+          components={customizableComponents("dark")}
+          className={"block md:hidden mt-8"}
+        >
+          {FooterData.copyright}
+        </ReactMarkdown>
       </div>
       <div className="w-full h-3 bg-primary-500" />
     </footer>
